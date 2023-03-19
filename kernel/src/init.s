1d28102ec5e63099901cd74e4825b925c98d620b
Gareth Morgan <gmorgan1984@gmail.com>
2023-03-19 09:35:17 +0000

    Added comment explaining that rdi is passing the bootinfo

--- kernel/src/init.s
+++ kernel/src/init.s
@@ -6,6 +6,11 @@ stack_top:
 
     .section .text
     .global _start
+
+# Initialise the kernel stack and call kernel_main
+#
+# Arguments:
+# rdi - Pointer to the BootInfo struct. This is just passed onto kernel_main
 _start:
     movabs $stack_top, %rsp
     call kernel_main

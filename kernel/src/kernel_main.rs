91d5118f8264b158d1de5c886251b26e9fb45b61
Gareth Morgan <gmorgan1984@gmail.com>
2023-03-27 23:05:46 +0100

    Simplified the TEMP_ALLOC code

--- kernel/src/kernel_main.rs
+++ kernel/src/kernel_main.rs
@@ -2,7 +2,7 @@ use x86_64_hardware::memory::paging::{PageTableManager, FrameAllocator};
 use x86_64_hardware::{com1_println, devices::uart_16550::COM1};
 use x86_64_hardware::tables::*;
 
-use crate::memory::{TEMP_ALLOC, get_temp_allocator, TempAllocWrapper, FRAME_ALLOCATOR};
+use crate::memory::{TEMP_ALLOC, FRAME_ALLOCATOR, get_pmm_functions};
 
 
 #[panic_handler]
@@ -26,13 +26,13 @@ pub extern "C" fn kernel_main(bootinfo: *mut bootinfo::BootInfo) {
     let meminfo = unsafe { (*bootinfo).meminfo.move_out() };
 
     unsafe { TEMP_ALLOC.init(&meminfo.bitmap, meminfo.free_memory, meminfo.reserved_memory, meminfo.used_memory) };
-    FRAME_ALLOCATOR.set_mem_manager(TempAllocWrapper::get_pmm_functions());
+    FRAME_ALLOCATOR.set_mem_manager(get_pmm_functions());
     //TODO - Kernel crashes if we don't pre-allocate this buffer. We need to investigate this because it shouldn't crash
     FRAME_ALLOCATOR.fill_buffer();
 
     let page_table_manager = PageTableManager::new_from_cr3(unsafe { (*bootinfo).page_table_memory_offset});
     for index in 0..255usize {
-        page_table_manager.unmap_p4_index(index, & FRAME_ALLOCATOR);
+        page_table_manager.unmap_p4_index(index, &FRAME_ALLOCATOR);
     }
     com1_println!("After identity map cleared!");
 
--- kernel/src/memory/temp_allocator.rs
+++ kernel/src/memory/temp_allocator.rs
@@ -4,36 +4,19 @@ use super::PhysicalMemoryManagerFunctions;
 
 pub static TEMP_ALLOC: PageFrameAllocator = PageFrameAllocator::new_uninit();
 
-pub fn get_temp_allocator() -> TempAllocWrapper {
-    return TempAllocWrapper {};
+
+pub fn get_pmm_functions() -> PhysicalMemoryManagerFunctions {
+    return PhysicalMemoryManagerFunctions::new(bulk_alloc, free);
 }
 
-pub struct TempAllocWrapper {}
-
-impl TempAllocWrapper {
-    pub fn get_pmm_functions() -> PhysicalMemoryManagerFunctions {
-        return PhysicalMemoryManagerFunctions::new(TempAllocWrapper::bulk_alloc, TempAllocWrapper::free);
+pub fn bulk_alloc(store: &mut[PhysicalAddress], count: usize) -> usize {
+    for index in 0..count {
+    store[index] = TEMP_ALLOC.request_page();
     }
-
-    pub fn bulk_alloc(store: &mut[PhysicalAddress], count: usize) -> usize {
-        for index in 0..count {
-            store[index] = TEMP_ALLOC.request_page();
-        }
-
-        return count;
-    }
-
-    pub fn free(address: PhysicalAddress) {
-        TEMP_ALLOC.free_page(address);
-    }
+    return count;
 }
 
-impl FrameAllocator for TempAllocWrapper {
-    fn free_page(&self, address: x86_64_hardware::memory::PhysicalAddress) {
-        TEMP_ALLOC.free_page(address);
-    }
-
-    fn request_page(&self) -> x86_64_hardware::memory::PhysicalAddress {
-        return TEMP_ALLOC.request_page();
-    }
+pub fn free(address: PhysicalAddress) {
+    TEMP_ALLOC.free_page(address);
 }
+
